1fd33d916a71f336261d8e3169ffb6f1fe2e574f
ShrutiRawal <shruti456rawal@gmail.com>
2019-12-30 15:27:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quizmaster/QuizDbHelper.java
@@ -0,0 +1,256 @@
+package com.example.quizmaster;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.quizmaster.QuizContract.CategoriesTable;
+
+import com.example.quizmaster.QuizContract.QuestionsTable;
+
+
+public class QuizDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "MyAwesomeQuiz.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static QuizDbHelper instance;
+
+    private SQLiteDatabase db;
+
+    private QuizDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public static synchronized QuizDbHelper getInstance(Context context) {
+        if (instance == null) {
+            instance = new QuizDbHelper(context.getApplicationContext());
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+
+        final String SQL_CREATE_CATEGORIES_TABLE = "CREATE TABLE " +
+                CategoriesTable.TABLE_NAME + "( " +
+                CategoriesTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                CategoriesTable.COLUMN_NAME + " TEXT " +
+                ")";
+
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuestionsTable.TABLE_NAME + " ( " +
+                QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuestionsTable.COLUMN_QUESTION + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
+                QuestionsTable.COLUMN_ANSWER_NR + " INTEGER, " +
+                QuestionsTable.COLUMN_DIFFICULTY + " TEXT, " +
+                QuestionsTable.COLUMN_CATEGORY_ID + " INTEGER, " +
+                "FOREIGN KEY(" + QuestionsTable.COLUMN_CATEGORY_ID + ") REFERENCES " +
+                CategoriesTable.TABLE_NAME + "(" + CategoriesTable._ID + ")" + "ON DELETE CASCADE" +
+                ")";
+
+        db.execSQL(SQL_CREATE_CATEGORIES_TABLE);
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillCategoriesTable();
+        fillQuestionsTable();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + CategoriesTable.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + QuestionsTable.TABLE_NAME);
+        onCreate(db);
+    }
+
+    @Override
+    public void onConfigure(SQLiteDatabase db) {
+        super.onConfigure(db);
+        db.setForeignKeyConstraintsEnabled(true);
+    }
+
+    private void fillCategoriesTable() {
+        Category c1 = new Category("Programming");
+        insertCategory(c1);
+        Category c2 = new Category("Geography");
+        insertCategory(c2);
+        Category c3 = new Category("Math");
+        insertCategory(c3);
+    }
+
+    public void addCategory(Category category) {
+        db = getWritableDatabase();
+        insertCategory(category);
+    }
+
+    public void addCategories(List<Category> categories) {
+        db = getWritableDatabase();
+
+        for (Category category : categories) {
+            insertCategory(category);
+        }
+    }
+
+    private void insertCategory(Category category) {
+        ContentValues cv = new ContentValues();
+        cv.put(CategoriesTable.COLUMN_NAME, category.getName());
+        db.insert(CategoriesTable.TABLE_NAME, null, cv);
+    }
+
+    private void fillQuestionsTable() {
+        Question q1 = new Question("Which of the following is a programming language?",
+                "C++", "Hindi", "Chinese", 1,
+                Question.DIFFICULTY_EASY, Category.PROGRAMMING);
+        insertQuestion(q1);
+        Question q2 = new Question("What is the name of capital of China?",
+                "Abio", "Beijing", "Xaiohing", 2,
+                Question.DIFFICULTY_MEDIUM, Category.GEOGRAPHY);
+        insertQuestion(q2);
+        Question q3 = new Question("What is the sum of probablities of all possible events in a sample space?",
+                "Uncertain", "0", "1", 3,
+                Question.DIFFICULTY_HARD, Category.MATH);
+        insertQuestion(q3);
+        Question q4 = new Question("2+2=?",
+                "4", "8", "16", 1,
+                Question.DIFFICULTY_EASY, Category.MATH);
+        insertQuestion(q4);
+        Question q5 = new Question("Non existing, Easy: A is correct",
+                "A", "B", "C", 1,
+                Question.DIFFICULTY_EASY, 4);
+        insertQuestion(q5);
+        Question q6 = new Question("Non existing, Medium: B is correct",
+                "A", "B", "C", 2,
+                Question.DIFFICULTY_MEDIUM, 5);
+        insertQuestion(q6);
+        Question q7 = new Question("Which of the following states of India have alluvial soil?",
+                "Uttar Pradesh","Rajasthan","Maharashtra",1,
+                Question.DIFFICULTY_EASY,Category.GEOGRAPHY);
+        insertQuestion(q7);
+        Question q8 = new Question("Which of the following is an object-oriented language?",
+                "C++","C","Ruby",1,
+                Question.DIFFICULTY_MEDIUM,Category.PROGRAMMING);
+        insertQuestion(q8);
+        Question q9 = new Question("Probablity of an event can never be -",
+                "less than 1","equal to 1","greater than 1",3,
+                Question.DIFFICULTY_MEDIUM,Category.MATH);
+        insertQuestion(q9);
+        Question q10 = new Question("Which of the following is not a feature of object-oriented programming language only?",
+                "Polymorphism","Objects","No data hiding",3,
+                Question.DIFFICULTY_HARD,Category.PROGRAMMING);
+        insertQuestion(q10);
+        Question q11 = new Question("Which  of the following cities is called The Pink City of India?",
+                "Raipur","Jaipur","Bhopal",2,
+                Question.DIFFICULTY_HARD,Category.GEOGRAPHY);
+        insertQuestion(q11);
+    }
+
+    public void addQuestion(Question question) {
+        db = getWritableDatabase();
+        insertQuestion(question);
+    }
+
+    public void addQuestions(List<Question> questions) {
+        db = getWritableDatabase();
+
+        for (Question question : questions) {
+            insertQuestion(question);
+        }
+    }
+
+    private void insertQuestion(Question question) {
+        ContentValues cv = new ContentValues();
+        cv.put(QuestionsTable.COLUMN_QUESTION, question.getQuestion());
+        cv.put(QuestionsTable.COLUMN_OPTION1, question.getOption1());
+        cv.put(QuestionsTable.COLUMN_OPTION2, question.getOption2());
+        cv.put(QuestionsTable.COLUMN_OPTION3, question.getOption3());
+        cv.put(QuestionsTable.COLUMN_ANSWER_NR, question.getAnswerNr());
+        cv.put(QuestionsTable.COLUMN_DIFFICULTY, question.getDifficulty());
+        cv.put(QuestionsTable.COLUMN_CATEGORY_ID, question.getCategoryID());
+        db.insert(QuestionsTable.TABLE_NAME, null, cv);
+    }
+
+    public List<Category> getAllCategories() {
+        List<Category> categoryList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + CategoriesTable.TABLE_NAME, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Category category = new Category();
+                category.setId(c.getInt(c.getColumnIndex(CategoriesTable._ID)));
+                category.setName(c.getString(c.getColumnIndex(CategoriesTable.COLUMN_NAME)));
+                categoryList.add(category);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return categoryList;
+    }
+
+    public ArrayList<Question> getAllQuestions() {
+        ArrayList<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + QuestionsTable.TABLE_NAME, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Question question = new Question();
+                question.setId(c.getInt(c.getColumnIndex(QuestionsTable._ID)));
+                question.setQuestion(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION3)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionsTable.COLUMN_ANSWER_NR)));
+                question.setDifficulty(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_DIFFICULTY)));
+                question.setCategoryID(c.getInt(c.getColumnIndex(QuestionsTable.COLUMN_CATEGORY_ID)));
+                questionList.add(question);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return questionList;
+    }
+
+    public ArrayList<Question> getQuestions(int categoryID, String difficulty) {
+        ArrayList<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+
+        String selection = QuestionsTable.COLUMN_CATEGORY_ID + " = ? " +
+                " AND " + QuestionsTable.COLUMN_DIFFICULTY + " = ? ";
+        String[] selectionArgs = new String[]{String.valueOf(categoryID), difficulty};
+
+        Cursor c = db.query(
+                QuestionsTable.TABLE_NAME,
+                null,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null
+        );
+
+        if (c.moveToFirst()) {
+            do {
+                Question question = new Question();
+                question.setId(c.getInt(c.getColumnIndex(QuestionsTable._ID)));
+                question.setQuestion(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION3)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionsTable.COLUMN_ANSWER_NR)));
+                question.setDifficulty(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_DIFFICULTY)));
+                question.setCategoryID(c.getInt(c.getColumnIndex(QuestionsTable.COLUMN_CATEGORY_ID)));
+                questionList.add(question);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return questionList;
+    }
+}
